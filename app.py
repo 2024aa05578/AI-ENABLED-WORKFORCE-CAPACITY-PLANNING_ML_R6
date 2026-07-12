78dc7f29ea2f21f7e5be9bfd4b5827b05e5cc1b9
2024aa05578 <2024aa05578@wilp.bits-pilani.ac.in>
2026-07-12 11:03:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-
 from workforce_model import calculate_workforce
-from ml_forecast import forecast_work_orders
 
 # =====================================================
 # PAGE CONFIGURATION
@@ -15,61 +13,70 @@ st.set_page_config(
 )
 
 # =====================================================
-# SIDEBAR - BU PARAMETERS
+# BU PARAMETERS
 # =====================================================
 
 st.sidebar.title("BU Wise Planning Parameters")
 
 bu_parameters = {}
 
+# UPS
+
 with st.sidebar.expander("UPS", expanded=True):
 
     bu_parameters["UPS"] = {
-
         "BAU": st.slider(
             "UPS BAU Growth %",
-            0, 100, 25,
+            0,
+            100,
+            25,
             key="ups_bau"
         ),
-
         "DC": st.slider(
             "UPS DC Growth %",
-            0, 100, 40,
+            0,
+            100,
+            40,
             key="ups_dc"
         ),
-
         "Attrition": st.slider(
             "UPS Attrition %",
-            0, 30, 8,
+            0,
+            30,
+            8,
             key="ups_attr"
         )
     }
 
+# Cooling
+
 with st.sidebar.expander("Cooling"):
 
     bu_parameters["Cooling"] = {
-
         "BAU": st.slider(
             "Cooling BAU Growth %",
-            0, 100, 20,
+            0,
+            100,
+            20,
             key="cool_bau"
         ),
-
         "DC": st.slider(
             "Cooling DC Growth %",
-            0, 100, 50,
+            0,
+            100,
+            50,
             key="cool_dc"
         ),
-
         "Attrition": st.slider(
             "Cooling Attrition %",
-            0, 30, 8,
+            0,
+            30,
+            8,
             key="cool_attr"
         )
     }
 
+# Power Products
+
 with st.sidebar.expander("Power Products"):
 
-    bu_parameters["Power Products"] = {
-
-        "BAU": st.slider(
