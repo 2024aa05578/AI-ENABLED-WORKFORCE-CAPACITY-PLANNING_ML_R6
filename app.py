41833ff8b366cc419d4ee0a1bb348ba053d59dd4
2024aa05578 <2024aa05578@wilp.bits-pilani.ac.in>
2026-07-12 11:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,17 +20,13 @@ st.sidebar.header("📈 North Region Growth")
 
 north_bau = st.sidebar.slider(
     "North BAU Growth %",
-    0,
-    100,
-    15,
+    0, 100, 15,
     key="north_bau"
 )
 
 north_dc = st.sidebar.slider(
     "North DC Growth %",
-    0,
-    100,
-    10,
+    0, 100, 10,
     key="north_dc"
 )
 
@@ -38,17 +34,13 @@ st.sidebar.header("📈 West Region Growth")
 
 west_bau = st.sidebar.slider(
     "West BAU Growth %",
-    0,
-    100,
-    20,
+    0, 100, 20,
     key="west_bau"
 )
 
 west_dc = st.sidebar.slider(
     "West DC Growth %",
-    0,
-    100,
-    40,
+    0, 100, 40,
     key="west_dc"
 )
 
@@ -56,35 +48,16 @@ st.sidebar.header("📈 South Region Growth")
 
 south_bau = st.sidebar.slider(
     "South BAU Growth %",
-    0,
-    100,
-    18,
+    0, 100, 18,
     key="south_bau"
 )
 
 south_dc = st.sidebar.slider(
     "South DC Growth %",
-    0,
-    100,
-    35,
+    0, 100, 35,
     key="south_dc"
 )
 
 st.sidebar.header("📈 East Region Growth")
 
 east_bau = st.sidebar.slider(
-    "East BAU Growth %",
-    0,
-    100,
-    10,
-    key="east_bau"
-)
-
-east_dc = st.sidebar.slider(
-    "East DC Growth %",
-    0,
-    100,
-    5,
-    key="east_dc"
-)
-
